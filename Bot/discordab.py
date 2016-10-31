11198886db5ab236e1bcb0d6147409cef61ca9ce
jbdo99 <jbwolf@hotmail.fr>
2016-10-31 11:08:49 +0100

    Update discordab.py

---
+++ Bot/discordab.py
@@ -0,0 +1,38 @@
+"""Copyright (C) 2016  Jbdo99
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+"""
+import asyncio
+import discord
+import logging
+import os
+import time
+import sys
+import youtube-dl
+from discord import opus
+
+OPUS_LIBS = ['libopus-0.x86.dll', 'libopus-0.x64.dll', 'libopus-0.dll', 'libopus.so.0', 'libopus.0.dylib']
+
+
+def load_opus_lib(opus_libs=OPUS_LIBS):
+    if opus.is_loaded():
+        return True
+
+    for opus_lib in opus_libs:
+        try:
+            opus.load_opus(opus_lib)
+            return
+        except OSError:
+            client.captureException()
+            pass
