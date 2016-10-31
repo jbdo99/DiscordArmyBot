bed2380c70f80761623a8d941a2748427fd7b411
jbdo99 <jbwolf@hotmail.fr>
2016-10-31 11:24:17 +0100

    Ajout encore des bases

--- Bot/discordab.py
+++ Bot/discordab.py
@@ -21,6 +21,10 @@ import time
 import sys
 import youtube-dl
 from discord import opus
+logging.basicConfig(level=logging.INFO)
+logging.basicConfig(filename='log.txt',level=logging.INFO)
+client = discord.Client()
+servers = list(client.servers)
 
 OPUS_LIBS = ['libopus-0.x86.dll', 'libopus-0.x64.dll', 'libopus-0.dll', 'libopus.so.0', 'libopus.0.dylib']
 
@@ -36,3 +40,35 @@ def load_opus_lib(opus_libs=OPUS_LIBS):
         except OSError:
             client.captureException()
             pass
+
+@client.async_event
+def on_ready():
+    print('Connected on discord')
+    print(client.user.name)
+    print("id : ",client.user.id)
+    yield from client.change_presence(game=discord.Game(name="Ready")
+    print ('Ready')
+    return
+
+
+
+@client.async_event
+def on_resumed():
+    logging.info('Resumed')
+    hard = discord.Object(id="178882236324642816")
+    yield from client.send_message(hard, "Resumed")
+
+@client.async_event
+def on_member_remove(member):
+    yield from client.send_message(member, 'Byeeeee ;)')
+
+@client.async_event
+def on_member_join(member):
+    yield from client.send_message(member, 'Salut :)')
+
+
+@client.async_event
+def on_server_remove(server):
+    logging.warning('un serveur a été supprimer de la liste des serveur')
+
+
