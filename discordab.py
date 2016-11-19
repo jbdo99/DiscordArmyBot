bda728bad6472e02f2402c8b182f9a5c630e6553
jbdo99 <jbwolf@hotmail.fr>
2016-11-19 12:02:23 +0100

    La zoubida3

--- discordab.py
+++ discordab.py
@@ -35,18 +35,7 @@ else:
 
 OPUS_LIBS = ['libopus-0.x86.dll', 'libopus-0.x64.dll', 'libopus-0.dll', 'libopus.so.0', 'libopus.0.dylib']
 
-
-def opus.load_opus_lib(opus_libs=OPUS_LIBS):
-    if opus.is_loaded():
-        return True
-
-    for opus_lib in opus_libs:
-        try:
-            opus.load_opus(opus_lib)
-            return
-        except OSError:
-        	print(OSError)
-            pass
+opus.load_opus(OPUS_LIBS)
 
 def is_me(m):
     return m.author == client.user
